8c40b324857beae3defd00a24d8a2fe6bfcf3df0
Oksana Iskandarova <94625357+OksanaIs@users.noreply.github.com>
2022-01-18 17:46:57 +0400

    Arithmetic

--- homework_1.py
+++ homework_1.py
@@ -1,117 +1,117 @@
-#1. Create a variable item_1 of type integer
+# 1. Create a variable item_1 of type integer
 item_1 = 15
 
-#2. Create variable item_2 of type integer
+# 2. Create variable item_2 of type integer
 item_2 = 25
 
-#3. Create a variable result_sum where you sum item_1 and item_2
+# 3. Create a variable result_sum where you sum item_1 and item_2
 result_sum = item_1 + item_2
 
-#4. Print result_sum to the console
+# 4. Print result_sum to the console
 print('result_sum = ', result_sum)
 
-#5. Create a result_subtr variable where you subtract the larger variable from the smaller one
+# 5. Create a result_subtr variable where you subtract the larger variable from the smaller one
 result_subtr = item_2 - item_1
 
-#6. Print result_subtr to the console
+# 6. Print result_subtr to the console
 print('result_subtr = ', result_subtr)
 
-#7. Create a variable result_multi where you multiply item_1 by item_2
+# 7. Create a variable result_multi where you multiply item_1 by item_2
 result_multi = item_1 * item_2
 
-#8. Print result_multi to the console
+# 8. Print result_multi to the console
 print('result_multi = ', result_multi)
 
-#9. Create a variable result_exp in which you raise item_1 to the power of item_2
+# 9. Create a variable result_exp in which you raise item_1 to the power of item_2
 result_exp = item_1 ** item_2
 
-#10. Print result_exp to the console
+# 10. Print result_exp to the console
 print('result_exp = ', result_exp)
 
-#11. Create a variable result_m_exp in which you raise item_1 to the power of item_2 using the math library
+# 11. Create a variable result_m_exp in which you raise item_1 to the power of item_2 using the math library
 import math
 result_m_exp = math.pow(item_1, item_2)
 
-#12. Print result_m_exp to the console
+# 12. Print result_m_exp to the console
 print('result_m_exp = ', result_m_exp)
 
-#13. Create a variable result_s_root in which you will find the square root of any of the variable item
+# 13. Create a variable result_s_root in which you will find the square root of any of the variable item
 result_s_root = item_2 ** 0.5
 
-#14. Print result_s_root to the console
+# 14. Print result_s_root to the console
 print('result_s_root = ', result_s_root)
 
-#15. Create a variable result_m_s_root in which you will find the square root of any of the item variable using the math library
+# 15. Create a variable result_m_s_root in which you will find the square root of any of the item variable using the math library
 result_m_s_root = math.sqrt(item_2)
 
-#16. Print result_m_s_root to the console
+# 16. Print result_m_s_root to the console
 print('result_m_s_root = ', result_m_s_root)
 
-#17. Create a variable result_mp_s_root in which you will find the square root of any of the item variable using the math library using the pow method
+# 17. Create a variable result_mp_s_root in which you will find the square root of any of the item variable using the math library using the pow method
 result_mp_s_root = math.pow(item_1, 0.5)
 
-#18. Print result_mp_s_root to the console
+# 18. Print result_mp_s_root to the console
 print('result_mp_s_root = ', result_mp_s_root)
 
 #19. Set variable item_1 odd to a value
 item_1 = 21
 
-#20. Assign variable item_2 even a value
+# 20. Assign variable item_2 even a value
 item_2 = 18
 
-#21. Create a variable result_division where you divide item_1 by item_2
+# 21. Create a variable result_division where you divide item_1 by item_2
 result_division = item_1 / item_2
 
-#22. Print result_division to the console
+# 22. Print result_division to the console
 print('result_division = ', result_division)
 
-#23. Create a variable result_m_floor and round result_division to the nearest integer less than result_division
+# 23. Create a variable result_m_floor and round result_division to the nearest integer less than result_division
 result_m_floor = math.floor(result_division)
 
-#24. Print result_m_floor to the console
+# 24. Print result_m_floor to the console
 print('result_m_floor = ', result_m_floor)
 
-#25. Create a variable result_m_ceil and round up result_division to the nearest integer greater than result_division
+# 25. Create a variable result_m_ceil and round up result_division to the nearest integer greater than result_division
 result_m_ceil = math.ceil(result_division)
 
-#26. Print result_m_ceil to the console
+# 26. Print result_m_ceil to the console
 print('result_m_ceil = ', result_m_ceil)
 
-#27. Create a variable result_int and round result_division to the nearest integer via an explicit cast
+# 27. Create a variable result_int and round result_division to the nearest integer via an explicit cast
 result_int = int(result_division)
 
-#28. Print result_int to console
+# 28. Print result_int to console
 print('result_int = ', result_int)
 
-#29. Create a variable result_no_division_loss in which you will divide item_1 by item_2 without a remainder
+# 29. Create a variable result_no_division_loss in which you will divide item_1 by item_2 without a remainder
 result_no_division_loss = item_1 // item_2
 
-#30. Print result_no_division_loss to the console
+# 30. Print result_no_division_loss to the console
 print('result_no_division_loss = ', result_no_division_loss)
 
-#31. Create a variable result_division_loss in which you will find the remainder of dividing item_1 by item_2
+# 31. Create a variable result_division_loss in which you will find the remainder of dividing item_1 by item_2
 result_division_loss = item_1 % item_2
 
-#32. Print result_division_loss to the console
+# 32. Print result_division_loss to the console
 print('result_division_loss = ', result_division_loss)
 
-#Further there will be an implementation through arithmetic operations with assignment.
-#33. Create variable item_3 and assign it an integer
+# Further there will be an implementation through arithmetic operations with assignment.
+# 33. Create variable item_3 and assign it an integer
 item_3 = 300
 
-#34. Add 10 to item_3 with assignment
+# 34. Add 10 to item_3 with assignment
 item_3 += 10
 
-#35. Print item_3 to the console
+# 35. Print item_3 to the console
 print('item_3 = ', item_3)
 
-#36. Subtract 5 from item_3 with assignment
+# 36. Subtract 5 from item_3 with assignment
 item_3 -= 5
 
-#37. Print item_3 to the console
+# 37. Print item_3 to the console
 print('item_3 = ', item_3)
 
-#38. Multiply item_3 by 3 with assignment
+# 38. Multiply item_3 by 3 with assignment
 item_3 *= 3
 
 #39. Print item_3 to the console
@@ -120,66 +120,66 @@ print('item_3 = ', item_3)
 #40. Divide item_3 by 2 with assignment
 item_3 /= 2
 
-#41. Print item_3 to the console
+# 41. Print item_3 to the console
 print('item_3 = ', item_3)
 
-#42. Raise to the power of 2 item_3 with assignment
+# 42. Raise to the power of 2 item_3 with assignment
 item_3 **= 2
 
-#43. Print item_3 to the console
+# 43. Print item_3 to the console
 print('item_3 = ', item_3)
 
-#44. Find the square root of item_3 with an assignment
+# 44. Find the square root of item_3 with an assignment
 item_3 = math.sqrt(item_3)
 
-#45. Print item_3 to the console
+# 45. Print item_3 to the console
 print('item_3 = ', item_3)
 
-#46. Assign modulo item_3
+# 46. Assign modulo item_3
 item_3 %= item_3
 
-#47. Print item_3 to the console
+# 47. Print item_3 to the console
 print('item_3 = ', item_3)
 
-#Boolean
-#48. Create variable b_item_t and set to True
+# Boolean
+# 48. Create variable b_item_t and set to True
 b_item_t = True
 
-#49. Create variable b_item_f and set to False
+# 49. Create variable b_item_f and set to False
 b_item_f = False
 
-#50. Create variable b_item_result_sum and assign the sum of b_item_t and b_item_f
+# 50. Create variable b_item_result_sum and assign the sum of b_item_t and b_item_f
 b_item_result_sum = b_item_t + b_item_f
 
-#51. Print b_item_relult_sum to the console
+# 51. Print b_item_relult_sum to the console
 print('b_item_result_sum = ', b_item_result_sum)
 
-#52. Create variable b_item_relult_subtr and assign difference between b_item_t and b_item_f
+# 52. Create variable b_item_relult_subtr and assign difference between b_item_t and b_item_f
 b_item_result_subtr = b_item_t - b_item_f
 
-#53. Print b_item_relult_subtr to the console
+# 53. Print b_item_relult_subtr to the console
 print('b_item_result_subtr = ', b_item_result_subtr)
 
-#54. Create variable b_item_relult_multi and assign multiplication to b_item_t and b_item_f
+# 54. Create variable b_item_relult_multi and assign multiplication to b_item_t and b_item_f
 b_item_result_multi = b_item_t * b_item_f
 
-#55. Print b_item_result_multi to console
+# 55. Print b_item_result_multi to console
 print('b_item_result_multi = ', b_item_result_multi)
 
-#56. Create variable b_item_relult_division and assign division to b_item_t and b_item_f
-#b_item_result_division = b_item_t / b_item_f
+# 56. Create variable b_item_relult_division and assign division to b_item_t and b_item_f
+# b_item_result_division = b_item_t / b_item_f
 
-#57. Print b_item_relult_division to the console (Get error)
-#print('b_item_result_division = ', b_item_result_division)
+# 57. Print b_item_relult_division to the console (Get error)
+# print('b_item_result_division = ', b_item_result_division)
 
-#58. Create variable b_item_1_int and assign explicit cast from b_item_t to int
+# 58. Create variable b_item_1_int and assign explicit cast from b_item_t to int
 b_item_1_int = int(b_item_t)
 
-#59. Print b_item_1_int to the console
+# 59. Print b_item_1_int to the console
 print('b_item_1_int = ', b_item_1_int)
 
-#60. Create variable b_item_2_int and assign explicit cast from b_item_f to int
+# 60. Create variable b_item_2_int and assign explicit cast from b_item_f to int
 b_item_f_int = int(b_item_f)
 
-#61. Print b_item_2_int to the console
+# 61. Print b_item_2_int to the console
 print('b_item_2_int = ', b_item_f_int)
